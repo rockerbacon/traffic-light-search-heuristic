e8f721867ae55ee71aece1ae7d1a676ec5cba472
Vitor Santos <vitor.asantos@b2wdigital.com>
2019-06-24 13:24:02 -0300

    Added cycle and minVertexDegree parameters to local search benchmark

--- src/benchmark_local_search.cpp
+++ src/benchmark_local_search.cpp
@@ -3,12 +3,12 @@
 #include <cstring>
 
 #define DEFAULT_NUMBER_OF_VERTICES 500
-#define DEFAULT_NUMBER_OF_RUNS 10
+#define DEFAULT_NUMBER_OF_RUNS 100
 #define DEFAULT_MAX_VERTEX_DEGREE 10
-#define DEFAULT_CYCLE 20
+#define DEFAULT_MIN_VERTEX_DEGREE 5
+#define DEFAULT_CYCLE 22
 #define DEFAULT_GRAPH_MODEL GraphModel::ADJACENCY_LIST
-#define DEFAULT_NUMBER_OF_PERTURBATIONS 1
-#define DEFAULT_STOP_CRITERIA stop_criteria::numberOfIterations(100000)
+#define DEFAULT_STOP_CRITERIA stop_criteria::numberOfIterations(10000)
 
 #define WRONG_ARGUMENTS_EXIT_CODE 1
 
@@ -21,8 +21,9 @@ enum GraphModel {
 	ADJACENCY_MATRIX
 };
 
-void setupExecutionParameters (int argc, char** argv, size_t &numberOfVertices, size_t &maxVertexDegree, unsigned &numberOfRuns, TimeUnit &cycle, GraphModel &graphModel, function<bool(const LocalSearchMetrics&)>& stopCriteriaNotMet) {
+void setupExecutionParameters (int argc, char** argv, size_t &numberOfVertices, size_t &minVertexDegree, size_t &maxVertexDegree, unsigned &numberOfRuns, TimeUnit &cycle, GraphModel &graphModel, function<bool(const LocalSearchMetrics&)>& stopCriteriaNotMet) {
 	numberOfVertices = DEFAULT_NUMBER_OF_VERTICES;
+	minVertexDegree = DEFAULT_MIN_VERTEX_DEGREE;
 	maxVertexDegree = DEFAULT_MAX_VERTEX_DEGREE;
 	numberOfRuns = DEFAULT_NUMBER_OF_RUNS;
 	cycle = DEFAULT_CYCLE;
@@ -71,6 +72,19 @@ void setupExecutionParameters (int argc, char** argv, size_t &numberOfVertices, 
 					exit(WRONG_ARGUMENTS_EXIT_CODE);
 				}
 
+			} else if (strcmp(argv[i], "--minVertexDegree") == 0) {
+
+				i++;
+				if (i >= argc) {
+					cout << "--minVertexDegree argument requires a number greater than 0" << endl;
+					exit(WRONG_ARGUMENTS_EXIT_CODE);
+				}
+				minVertexDegree = atoi(argv[i]);
+				if (maxVertexDegree == 0) {
+					cout << "--minVertexDegree argument requires a number greater than 0" << endl;
+					exit(WRONG_ARGUMENTS_EXIT_CODE);
+				}
+
 			} else if (strcmp(argv[i], "--useAdjacencyList") == 0) {
 
 				graphModel = GraphModel::ADJACENCY_LIST;
@@ -110,6 +124,20 @@ void setupExecutionParameters (int argc, char** argv, size_t &numberOfVertices, 
 					exit(WRONG_ARGUMENTS_EXIT_CODE);
 				}
 				stopCriteriaNotMet = stop_criteria::numberOfIterations(numberOfIterations);
+
+			} else if (strcmp(argv[i], "--cycle") == 0) {
+
+				i++;
+				if (i >= argc) {
+					cout << "--cycle argument requires a number greater than 0" << endl;
+					exit(WRONG_ARGUMENTS_EXIT_CODE);
+				}
+				cycle = atoi(argv[i]);
+				if (cycle == 0) {
+					cout << "--cycle argument requires a number greater than 0" << endl;
+					exit(WRONG_ARGUMENTS_EXIT_CODE);
+				}
+
 			} else {
 				cout << "unknown argument " << argv[i] << endl;
 				exit(WRONG_ARGUMENTS_EXIT_CODE);
@@ -125,7 +153,7 @@ int main (int argc, char** argv) {
 	GraphBuilder *graphBuilder = nullptr;
 	Graph *graph = nullptr;
 	Solution constructedSolution, searchedSolution;
-	size_t numberOfVertices, maxVertexDegree;
+	size_t numberOfVertices, maxVertexDegree, minVertexDegree;
 	unsigned numberOfRuns;
 	TimeUnit cycle;
 	GraphModel graphModel;
@@ -138,7 +166,7 @@ int main (int argc, char** argv) {
 	chrono::high_resolution_clock::duration avgSearchDuration;
 	string formatedAvgSearchDuration;
 
-	setupExecutionParameters(argc, argv, numberOfVertices, maxVertexDegree, numberOfRuns, cycle, graphModel, stopCriteriaNotMet);
+	setupExecutionParameters(argc, argv, numberOfVertices, minVertexDegree, maxVertexDegree, numberOfRuns, cycle, graphModel, stopCriteriaNotMet);
 
 	terminalObserver = new TerminalObserver("local search heuristic", numberOfRuns);
 	terminalObserver->observeVariable("graph lower bound", avgLowerBound);
@@ -153,7 +181,7 @@ int main (int argc, char** argv) {
 	avgLocalSearchPenalty = 0;
 	for (auto o : observers) o->notifyBenchmarkBegun();
 	for (unsigned i = 0; i < numberOfRuns; i++) {
-		graphBuilder = new GraphBuilder(numberOfVertices, 1, maxVertexDegree, 1, cycle-1);
+		graphBuilder = new GraphBuilder(numberOfVertices, minVertexDegree, maxVertexDegree, 1, cycle-1);
 		graphBuilder->withCycle(cycle);
 		switch (graphModel) {
 			case GraphModel::ADJACENCY_LIST:
