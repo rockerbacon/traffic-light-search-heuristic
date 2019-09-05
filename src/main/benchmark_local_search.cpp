614c0a7642d9827968d43ee7ae8b2467ef0fe886
rockerbacon <vas.vitor.santos@gmail.com>
2019-09-05 15:35:16 -0300

    Updated benchmark_local_search to use new CLI framework

--- src/main/benchmark_local_search.cpp
+++ src/main/benchmark_local_search.cpp
@@ -1,169 +1,67 @@
 #include "heuristic/heuristic.h"
 #include "assertions/benchmark.h"
+#include "assertions/command_line_interface.h"
 #include <cstring>
 
-#define DEFAULT_NUMBER_OF_VERTICES 500
 #define DEFAULT_NUMBER_OF_RUNS 100
-#define DEFAULT_MAX_VERTEX_DEGREE 10
-#define DEFAULT_MIN_VERTEX_DEGREE 4
-#define DEFAULT_CYCLE 24
 #define DEFAULT_GRAPH_MODEL GraphModel::ADJACENCY_LIST
-#define DEFAULT_STOP_CRITERIA stop_criteria::numberOfIterations(10000)
-
-#define WRONG_ARGUMENTS_EXIT_CODE 1
+#define DEFAULT_STOP_FUNCTION stop_function_factory::numberOfIterations(10000)
 
 using namespace std;
 using namespace traffic;
 using namespace benchmark;
-
-enum GraphModel {
-	ADJACENCY_LIST,
-	ADJACENCY_MATRIX
-};
-
-void setupExecutionParameters (int argc, char** argv, size_t &numberOfVertices, size_t &minVertexDegree, size_t &maxVertexDegree, unsigned &numberOfRuns, TimeUnit &cycle, GraphModel &graphModel, function<bool(const HeuristicMetrics&)>& stopCriteriaNotMet) {
-	numberOfVertices = DEFAULT_NUMBER_OF_VERTICES;
-	minVertexDegree = DEFAULT_MIN_VERTEX_DEGREE;
-	maxVertexDegree = DEFAULT_MAX_VERTEX_DEGREE;
-	numberOfRuns = DEFAULT_NUMBER_OF_RUNS;
-	cycle = DEFAULT_CYCLE;
-	graphModel = DEFAULT_GRAPH_MODEL;
-	stopCriteriaNotMet = DEFAULT_STOP_CRITERIA;
-
-	if (argc > 1) {
-		int i = 1;
-		while (i < argc) {
-			if (strcmp(argv[i], "--vertices") == 0) {
-
-				i++;
-				if (i >= argc) {
-					cout << "--vertices argument requires a number greater than 0" << endl;
-					exit(WRONG_ARGUMENTS_EXIT_CODE);
-				}
-				numberOfVertices = atoi(argv[i]);
-				if (numberOfVertices == 0) {
-					cout << "--vertices argument requires a number greater than 0" << endl;
-					exit(WRONG_ARGUMENTS_EXIT_CODE);
-				}
-
-			} else if (strcmp(argv[i], "--runs") == 0) {
-
-				i++;
-				if (i >= argc) {
-					cout << "--runs argument requires a number greater than 0" << endl;
-					exit(WRONG_ARGUMENTS_EXIT_CODE);
-				}
-				numberOfRuns = atoi(argv[i]);
-				if (numberOfRuns == 0) {
-					cout << "--runs argument requires a number greater than 0" << endl;
-					exit(WRONG_ARGUMENTS_EXIT_CODE);
-				}
-
-			} else if (strcmp(argv[i], "--maxVertexDegree") == 0) {
-
-				i++;
-				if (i >= argc) {
-					cout << "--maxVertexDegree argument requires a number greater than 0" << endl;
-					exit(WRONG_ARGUMENTS_EXIT_CODE);
-				}
-				maxVertexDegree = atoi(argv[i]);
-				if (maxVertexDegree == 0) {
-					cout << "--maxVertexDegree argument requires a number greater than 0" << endl;
-					exit(WRONG_ARGUMENTS_EXIT_CODE);
-				}
-
-			} else if (strcmp(argv[i], "--minVertexDegree") == 0) {
-
-				i++;
-				if (i >= argc) {
-					cout << "--minVertexDegree argument requires a number greater than 0" << endl;
-					exit(WRONG_ARGUMENTS_EXIT_CODE);
-				}
-				minVertexDegree = atoi(argv[i]);
-				if (maxVertexDegree == 0) {
-					cout << "--minVertexDegree argument requires a number greater than 0" << endl;
-					exit(WRONG_ARGUMENTS_EXIT_CODE);
-				}
-
-			} else if (strcmp(argv[i], "--useAdjacencyList") == 0) {
-
-				graphModel = GraphModel::ADJACENCY_LIST;
-
-			} else if (strcmp(argv[i], "--useAdjacencyMatrix") == 0) {
-
-				graphModel = GraphModel::ADJACENCY_MATRIX;
-
-			} else if (strcmp(argv[i], "--iterationsWithoutImprovement") == 0) {
-
-				unsigned numberOfIterations;
-
-				i++;
-				if (i >= argc) {
-					cout << "--iterationsWithoutImprovement argument requires a number greater than 0" << endl;
-					exit(WRONG_ARGUMENTS_EXIT_CODE);
-				}
-				numberOfIterations = atoi(argv[i]);
-				if (numberOfIterations == 0) {
-					cout << "--iterationsWithoutImprovement argument requires a number greater than 0" << endl;
-					exit(WRONG_ARGUMENTS_EXIT_CODE);
-				}
-				stopCriteriaNotMet = stop_criteria::numberOfIterationsWithoutImprovement(numberOfIterations);
-
-			} else if (strcmp(argv[i], "--iterations") == 0) {
-
-				unsigned numberOfIterations;
-
-				i++;
-				if (i >= argc) {
-					cout << "--iterations argument requires a number greater than 0" << endl;
-					exit(WRONG_ARGUMENTS_EXIT_CODE);
-				}
-				numberOfIterations = atoi(argv[i]);
-				if (numberOfIterations == 0) {
-					cout << "--iterations argument requires a number greater than 0" << endl;
-					exit(WRONG_ARGUMENTS_EXIT_CODE);
-				}
-				stopCriteriaNotMet = stop_criteria::numberOfIterations(numberOfIterations);
-
-			} else if (strcmp(argv[i], "--cycle") == 0) {
-
-				i++;
-				if (i >= argc) {
-					cout << "--cycle argument requires a number greater than 0" << endl;
-					exit(WRONG_ARGUMENTS_EXIT_CODE);
-				}
-				cycle = atoi(argv[i]);
-				if (cycle == 0) {
-					cout << "--cycle argument requires a number greater than 0" << endl;
-					exit(WRONG_ARGUMENTS_EXIT_CODE);
-				}
-
-			} else {
-				cout << "unknown argument " << argv[i] << endl;
-				exit(WRONG_ARGUMENTS_EXIT_CODE);
-			}
-
-			i++;
-		}
-	}
-}
+using namespace heuristic;
 
 int main (int argc, char** argv) {
 
-	GraphBuilder *graphBuilder = nullptr;
+	GraphBuilder graphBuilder;
 	Graph *graph = nullptr;
 	Solution constructedSolution, searchedSolution;
 	size_t numberOfVertices, maxVertexDegree, minVertexDegree;
-	unsigned numberOfRuns;
 	TimeUnit cycle;
-	GraphModel graphModel;
-	function<bool(const HeuristicMetrics&)> stopCriteriaNotMet;
+	StopFunction stopFunction;
 	double initialConstructionPenalty, localSearchPenalty, lowerBound;
 	double penaltyFactor, lowerBoundFactor;
 	chrono::high_resolution_clock::time_point beginSearch;
 	chrono::high_resolution_clock::duration searchDuration;
+	ifstream fileInputStream;
 
-	setupExecutionParameters(argc, argv, numberOfVertices, minVertexDegree, maxVertexDegree, numberOfRuns, cycle, graphModel, stopCriteriaNotMet);
+	/* CLI ARGUMENTS */
+	cli::RequiredArgument<string> inputFilePath("input", 'i');
+
+	cli::OptionalArgument<unsigned> numberOfRuns("runs", DEFAULT_NUMBER_OF_RUNS);
+
+	cli::FlagArgument useAdjacencyMatrix("useAdjacencyMatrix");
+	cli::FlagArgument useAdjacencyList("useAdjacencyList");
+
+	cli::OptionalArgument<unsigned> stopAfterNumberOfIterations("iterations", 0);
+	cli::OptionalArgument<unsigned> stopAfterIterationsWithoutImprovement("iterationsWithoutImprovement", 0);
+	cli::OptionalArgument<unsigned> stopAfterSeconds("seconds", 0);
+	cli::OptionalArgument<unsigned> stopAfterMinutes("minutes", 0);
+
+	fileInputStream.open(*inputFilePath);
+	graphBuilder.read_from_file(fileInputStream);
+
+	if (*useAdjacencyMatrix) {
+		graph = graphBuilder.buildAsAdjacencyMatrix();
+	} else {
+		graph = graphBuilder.buildAsAdjacencyList();
+	}
+
+	if (stopAfterNumberOfIterations.is_present()) {
+		stopFunction = stop_function_factory::numberOfIterations(*stopAfterNumberOfIterations);
+	} else if (stopAfterIterationsWithoutImprovement.is_present()) {
+		stopFunction = stop_function_factory::numberOfIterationsWithoutImprovement(*stopAfterIterationsWithoutImprovement);
+	} else if (stopAfterSeconds.ArgumentInterface::is_present()) {
+		stopFunction = stop_function_factory::executionTime(chrono::seconds(*stopAfterSeconds));
+	} else if (stopAfterMinutes.is_present()) {
+		stopFunction = stop_function_factory::executionTime(chrono::minutes(*stopAfterMinutes));
+	} else {
+		stopFunction = DEFAULT_STOP_FUNCTION;
+	}
+
+	cli::capture_all_arguments_from(argc, argv);
+	/* CLI ARGUMENTS */
 
 	register_observer(new TerminalObserver());
 
@@ -174,21 +72,11 @@ int main (int argc, char** argv) {
 	observe_variable("search/lower bound factor", lowerBoundFactor, observation_mode::AVERAGE_VALUE);
 	observe_variable("search duration", searchDuration, observation_mode::AVERAGE_VALUE);
 
-	benchmark("local search heuristic", numberOfRuns) {
-		graphBuilder = new GraphBuilder(numberOfVertices, minVertexDegree, maxVertexDegree, 1, cycle-1);
-		graphBuilder->withCycle(cycle);
-		switch (graphModel) {
-			case GraphModel::ADJACENCY_LIST:
-				graph = graphBuilder->buildAsAdjacencyList();
-				break;
-			case GraphModel::ADJACENCY_MATRIX:
-				graph = graphBuilder->buildAsAdjacencyMatrix();
-				break;
-		}
+	benchmark("local search heuristic", *numberOfRuns) {
 
 		beginSearch = chrono::high_resolution_clock::now();
 		constructedSolution = constructHeuristicSolution(*graph);
-		searchedSolution = localSearchHeuristic(*graph, constructedSolution, stopCriteriaNotMet);
+		searchedSolution = localSearchHeuristic(*graph, constructedSolution, stopFunction);
 
 		searchDuration = chrono::high_resolution_clock::now() - beginSearch;
 		initialConstructionPenalty = graph->totalPenalty(constructedSolution);
@@ -198,10 +86,9 @@ int main (int argc, char** argv) {
 		penaltyFactor = localSearchPenalty/initialConstructionPenalty;
 		lowerBoundFactor = localSearchPenalty/lowerBound;
 
-		delete graphBuilder;
-		delete graph;
 	} end_benchmark;
 
+	delete graph;
 	delete_observers();
 
 	return 0;
