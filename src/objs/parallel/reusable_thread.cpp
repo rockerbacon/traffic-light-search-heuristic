50a9058b15bb6ff5085caf38dfce9a24a83d1562
rockerbacon <vas.vitor.santos@gmail.com>
2019-11-20 20:44:04 -0300

    Changed reusable thread to never block

--- src/objs/parallel/reusable_thread.cpp
+++ src/objs/parallel/reusable_thread.cpp
@@ -5,15 +5,20 @@ using namespace parallel;
 
 reusable_thread::reusable_thread() :
 	running(true),
-	has_new_task(false),
+	tasks_count(0),
 	mutex(),
-	condition_variable(),
+	notifier(),
 	thread([this]() {
-		unique_lock<std::mutex> lock(this->mutex);
-		while(this->running) {
-			this->condition_variable.wait(lock, [this]() { return this->has_new_task; });
+		packaged_task<void()> current_task;
+		while(this->running || this->tasks_count > 0) {
+			{
+				unique_lock<std::mutex> lock(this->mutex);
+				this->notifier.wait(lock, [this]() { return this->tasks_count > 0; });
+				current_task = std::move(this->task_queue.front());
+				this->task_queue.pop_front();
+				this->tasks_count--;
+			}
 			current_task();
-			this->has_new_task = false;
 		}
 	})
 {}
@@ -24,23 +29,23 @@ reusable_thread::~reusable_thread() {
 	}
 }
 
-future<void> reusable_thread::exec(const function<void()> &task_function) {
-	packaged_task<void()> task(task_function);
-	auto future = task.get_future();
-
+future<void> reusable_thread::exec(const function<void()> &task) {
+	future<void> future;
 	{
 		lock_guard<std::mutex> lock(this->mutex);
-		swap(this->current_task, task);
-		this->has_new_task = true;
+		this->task_queue.emplace_back(task);
+		future = this->task_queue.back().get_future();
+		this->tasks_count++;
 	}
-	this->condition_variable.notify_one();
+
+	this->notifier.notify_one();
 
 	return future;
 }
 
 void reusable_thread::join() {
 	this->running = false;
-	this->exec({ /* empty task */});
+	this->exec([]{ /* empty task */ });
 	this->thread.join();
 }
 
--- src/objs/parallel/reusable_thread.h
+++ src/objs/parallel/reusable_thread.h
@@ -3,6 +3,7 @@
 #include <thread>
 #include <mutex>
 #include <future>
+#include <list>
 #include <functional>
 
 namespace parallel {
@@ -10,17 +11,17 @@ namespace parallel {
 	class reusable_thread {
 		private:
 			volatile bool running;
-			volatile bool has_new_task;
+			volatile unsigned tasks_count;
 			std::mutex mutex;
-			std::condition_variable condition_variable;
+			std::condition_variable notifier;
 			std::thread thread;
-			std::packaged_task<void()> current_task;
+			std::list<std::packaged_task<void()>> task_queue;
 
 		public:
 			reusable_thread();
 			~reusable_thread();
 
-			std::future<void> exec(const std::function<void()> &task_function);
+			std::future<void> exec(const std::function<void()> &task);
 			void join();
 			bool joinable() const;
 	};
